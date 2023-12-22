40707b52273fae4e33817e68e8ec7907fa762e65
nainoktv <nainoaktvillegas@gmail.com>
2023-12-21 16:56:45 -0800

    chore: pointers for targeted brackets

---
+++ index.py
@@ -0,0 +1,12 @@
+with open("story.txt", "r") as f:
+  story = f.read()
+
+words = []
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story):
+  if char == target_start:
+    start_of_word = i
