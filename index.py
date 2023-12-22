e28d02f747b27fa91c26a7cbf84f7a42c6cb7eb2
nainoktv <nainoaktvillegas@gmail.com>
2023-12-21 17:12:37 -0800

    chore: change list to a set

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 with open("story.txt", "r") as f:
   story = f.read()
 
-words = []
+words = set()
 start_of_word = -1
 
 target_start = "<"
@@ -10,3 +10,10 @@ target_end = ">"
 for i, char in enumerate(story):
   if char == target_start:
     start_of_word = i
+
+  if char == target_end and start_of_word != -1:
+    word = story[start_of_word: i + 1]
+    words.add(word)
+    start_of_word = -1
+
+answers = {}
